f6db4e72f2d9406e358e7ffc4592292c7ca9e462
Truman Fogler <tfogler@scc-wl2.scc.bu.edu>
2023-12-13 19:40:17 -0500

    UI Bieng maiud

--- app/app.R
+++ app/app.R
@@ -31,12 +31,12 @@ ui <- fluidPage(
        sidebarLayout(
           sidebarPanel(
               # Input: Select a file ----
-              fileInput("file", "Choose CSV File",
+              fileInput("samplefile", "Choose CSV File",
                         multiple = TRUE,
                         accept = c("text/csv",
                                    "text/comma-separated-values,text/plain",
                                    ".csv"),
-                        placeholder = "counts_data.csv"),
+                        placeholder = "example_intensity_data_subset_69.csv"),
               
               sliderInput("bins",
                          "Number of bins:",
@@ -45,24 +45,74 @@ ui <- fluidPage(
                          value = 30)
           ),
           
-          # Show a plot of the generated distribution
           mainPanel(
-             # plotOutput("distPlot")
-             tableOutput("countsTable")
+            tabsetPanel(
+                tabPanel("Summary", # Show a summary of the sample distribution
+                    p("Placeholder")
+                ),
+                
+                tabPanel("Table",
+                    p("Placeholder")
+                ),
+                
+                tabPanel("Plots",
+                )
+            )
           )
         )
       ),
      
      # Counts tab
      tabPanel("Counts",
-       # Sidebar
        sidebarLayout(
+           # Sidebar
+           
            sidebarPanel(
-               # stuff here
+               # Input: Select a file ----
+               fileInput("file", "Upload *NORMALIZED* Counts Matrix File",
+                         multiple = TRUE,
+                         accept = c("text/csv",
+                                    "text/comma-separated-values,text/plain",
+                                    ".csv"),
+                         placeholder = c("example_intensity_data_subset_69.csv")
+                         ),
+               
+               # HORIZONAL LINE
+               hr(),
+               
+               # Heading
+               h2("Filter Counts Data according to Statistical Params:"),
+               
+               # Input: Percent Variance Slider
+               sliderInput("var",
+                           "% Variance",
+                           min = 0,
+                           max = 100,
+                           value = 80
+               ),
+               
+               # Input: Min Number of Non-zero Samples Slider
+               sliderInput("zero",
+                           "Minimun Number of Non-Zero Samples:",
+                           min = 0,
+                           max = )
            ),
            
-           # Counts table panel
            mainPanel(
+              # Counts table panel
+              tabsetPanel(
+                  tabPanel("Summary",
+                      # Summary table
+                      p("Summary of Counts File"),
+                      tableOutput(outputId = "summaryTable")
+                  ),
+                  
+                  tabPanel(
+                      "Head",
+                      p("Counts File Table"),
+                      tableOutput(outputId = "countsTable")
+                  )
+              )
                # counts table
            )
        ),
@@ -74,6 +124,7 @@ ui <- fluidPage(
         sidebarLayout(
             sidebarPanel(
                 # more stuff
+                
             ),
             
             # DEs plot panel
@@ -84,16 +135,49 @@ ui <- fluidPage(
     )
 )
 
+
+
+
+
+
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-    
+    #' 
+    #' loads data and returns dataframe
     load_data <- reactive({
         return(read.csv(input$file$datapath))
     })
     
+    ## Server functions go here
+    
+    #' 
+    #' Summarize the data from input file
+    #' For column in data, display the name,
+    #' Datatype, and Mean or discrete
+    #' values depending if data are 
+    #' numerical or categorical.
+    #' 
+    data_summary <- function(input_data) {
+        summary.data.frame(input_data)
+    }
+    
+    ## Output Elements go here
+    
+    #' generate a table and display its head (first 6 rows)
     output$countsTable <- renderTable({
         # table
+        req(input$file)
         head(load_data())
+    })
+    
+    output$summaryTable <- renderTable({
+        # table
+        req(input$file)
+        
+        summary_table <- data_summary(
+            load_data())
+        
+        return(summary_table)
     })
     
     output$distPlot <- renderPlot({
@@ -104,6 +188,7 @@ server <- function(input, output) {
       # draw the histogram with the specified number of bins
       hist(x, breaks = bins, col = 'darkgray', border = 'white')
     })
+    
 }
 
 # Run the application 
